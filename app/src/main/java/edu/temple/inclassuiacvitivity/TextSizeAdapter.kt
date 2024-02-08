cfc6bfd595b4dc9645eb71be66b3b628291c9953
chris-douglas13 <111987005+chris-douglas13@users.noreply.github.com>
2024-02-08 11:40:32 -0500

    Custom TextSizeAdapter implemented. Font sizes correspond with Array ints but unable to see default spinner value.

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,33 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _textSize: Array<Int>) : BaseAdapter() {
+    private val context = _context
+    private val textSize = _textSize
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+       return textSize.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return textSize[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = textSize[position].toString()
+        textView.textSize = textSize[position].toFloat()
+        textView.setPadding(5,10,0,10)
+        return textView
     }
 
 }
